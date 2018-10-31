ae8e357deec6a5cad3787d661efe5b83c985415c
steebe <steve@stevebass.me>
2018-10-31 10:55:41 -0700

    Added firebase metadata and initialized firebase directory.

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: 'AIzaSyCs7GI93hx1zcjBkULmKLVwvCL4ulRT9Kw',
+  authDomain: 'nice-riff.firebaseapp.com',
+  databaseURL: 'https://nice-riff.firebaseio.com',
+  projectId: 'nice-riff',
+  storageBucket: 'nice-riff.appspot.com',
+  messagingSenderId: '429461910911',
 };
